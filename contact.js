42b05b4515f081b2f74f7f82aed629cd4c491c3b
Karnik <karnikpatel84@gmail.com>
2023-09-24 21:17:01 +0530

    karnik final

---
+++ contact.js
@@ -0,0 +1,21 @@
+function sendMail(){
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceID = "service_47aa2s7"
+    const templateID = "template_nhgc9mt"
+
+    emailjs.send(serviceID,templateID,params)
+    .then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message has been sent successfully");
+        })
+    .catch((err) => console.log(err));
+}
+
